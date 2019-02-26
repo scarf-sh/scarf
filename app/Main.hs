61d3b9ee3d10a84ce4e4ed06678236a7c4b3c455
aviaviavi <avipress@gmail.com>
2019-02-25 22:14:58 -0700

    args parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Lib
 
+import           Control.Monad.Reader
+import           Data.Semigroup       ((<>))
+import           Data.Text            (Text)
+import qualified Data.Text            as T
+import           Options.Applicative
+import           Prelude              hiding (FilePath)
+import           System.Environment
+
+data UArgs
+  = UInstall { file :: FilePath }
+  | UExecute { target :: Text
+             , args   :: Text } deriving (Show)
+
+installInput :: Parser UArgs
+installInput = UInstall <$> argument str
+  (
+  metavar "FILENAME"
+  <> help "Binary, script, etc to install with u" )
+
+executeInput :: Parser UArgs
+executeInput = UExecute <$> argument str
+  (
+  metavar "FILENAME"
+  <> help "Binary, script, etc to run with u" ) <*>
+  strOption (long "args" <> metavar "ARGS" <> help "args to pass to the target program")
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts description = info (helper <*> opts) $ progDesc description
+
+input :: Parser UArgs
+input = subparser $
+  command "install" (installInput `withInfo` "Install an executable with u") <>
+  command "execute" (executeInput `withInfo` "Run a u-installed executable")
+
+inputParserInfo = info (input <**> helper)
+      ( fullDesc
+     <> progDesc "U helps developer tool developers"
+     <> header "U, the command line wrapper" )
+
 main :: IO ()
-main = runProgramWrapped "~/side/u/u-test.sh" >>= print
+main = do
+  options <- execParser inputParserInfo
+  print options
+  home <- getEnv "HOME"
+  runReaderT (installProgramWrapped "~/side/u/u-test.sh") $ Config (toText home)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,43 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Lib where
+module Lib
+  (
+    FilePath,
+    Config(..),
+
+    toString,
+    toText,
+    runProgramWrapped,
+    installProgramWrapped
+  )
+where
 
 import           Control.Exception          (throwIO)
+import           Control.Monad.IO.Class
+import           Control.Monad.Reader
 import qualified Data.ByteString.Lazy       as L
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.Text                  (Text)
+import qualified Data.Text                  as T
+import           Data.Text.IO
 import           Data.Time.Clock.POSIX
+import           DynFlags
+import           Prelude                    hiding (FilePath, writeFile)
+import           System.Directory
 import           System.Exit
 import           System.IO                  (hClose, hPutStr)
 import           System.Process.Typed
 
+type FilePath = Text
+
+toString = T.unpack
+toText = T.pack
+
+data Config = Config {
+  homeDirectory :: FilePath
+                     }
 
 data ExecutionResult = ExecutionResult
   { result    :: ExitCode
@@ -19,7 +47,20 @@ data ExecutionResult = ExecutionResult
 runProgramWrapped :: FilePath -> IO ExecutionResult
 runProgramWrapped f = do
   start <- (round . (* 1000)) `fmap` getPOSIXTime
-  exitCode <- runProcess $ shell f
+  exitCode <- runProcess $ shell (T.unpack f)
   end <- (round . (* 1000)) `fmap` getPOSIXTime
   return $ ExecutionResult exitCode (end - start)
 
+originalProgram homeFolder fileName = homeFolder <>  "/.u/original/" <> fileName
+wrappedProgram homeFolder fileName = homeFolder <> "/.u/bin/" <> fileName
+
+installProgramWrapped :: (MonadReader Config m, MonadIO m) => FilePath -> m ()
+installProgramWrapped f =
+  let fileName = last $ T.splitOn "/" f in do
+    home <- homeDirectory <$> ask
+    liftIO $ copyFile (toString (T.replace "~" home f)) (toString $ originalProgram home fileName)
+    -- TODO(#bug) conflicting filenames breaks stuff
+    liftIO $ writeFile (toString $ wrappedProgram home fileName) (T.unlines ["#!/bin/bash",
+                                                             "args=\"'$*'\"",
+                                                             "./u-exe --file " <> (originalProgram home fileName) <> " $args"
+                                                                          ])
