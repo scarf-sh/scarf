6404d23ce145b167cb8a0172a6d05cee7c200853
aviaviavi <avipress@gmail.com>
2019-04-10 16:42:15 -0700

    configurable base urls, removed some old dependencies that were moved to the server repo

--- src/Scarf/PackageSpec.hs
+++ src/Scarf/PackageSpec.hs
@@ -18,29 +18,19 @@ module Scarf.PackageSpec where
 
 import           Scarf.Common
 
-import           Data.Aeson                      (FromJSON, ToJSON)
+import           Data.Aeson          (FromJSON, ToJSON)
 import           Data.Aeson.TH
 import           Data.SemVer
-import           Data.Text                       (Text)
-import qualified Data.Text                       as T
-import           Database.Beam.Backend.SQL.Row
-import           Database.Beam.Backend.SQL.SQL92
-import           Database.Beam.Backend.Types
-import           Database.Beam.Postgres
-import qualified Dhall                           as Dhall
+import           Data.Text           (Text)
+import qualified Data.Text           as T
+import qualified Dhall               as Dhall
 import           GHC.Generics
 import           Lens.Micro.Platform
-import           Prelude                         hiding (FilePath, writeFile)
+import           Prelude             hiding (FilePath, writeFile)
 
 data Platform = MacOS | Linux_i386 | Linux_x86_64 deriving (Show, Eq, Read, Generic, ToJSON, FromJSON)
 
 instance Dhall.Interpret Platform
-
-instance FromBackendRow Postgres Platform where
-  fromBackendRow = read . T.unpack <$> fromBackendRow
-
-instance HasSqlValueSyntax be String => HasSqlValueSyntax be Platform where
-  sqlValueSyntax = autoSqlValueSyntax
 
 -- no lenses because they don't play nice with dhall yet
 data PackageDistribution = PackageDistribution {
