4cb7d92c261f763240754a8d9b74ad9356e8dc43
Alex Biehl <alexbiehl@gmail.com>
2021-03-18 16:21:37 +0100

    Ormolufy

--- Main.hs
+++ Main.hs
@@ -1,28 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE TemplateHaskell, RecordWildCards #-}
-{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
-import Data.Aeson
-import Options.Applicative
-import System.Process
-import System.Exit
-import Data.Text hiding (init)
-import Development.Placeholders
-import System.Environment.XDG.BaseDir
-import System.Directory
-import Paths_scarf
 import Control.Monad
-import System.Environment
-import System.FilePath
-import Data.Functor
+import Data.Aeson
+import Data.Aeson.Encode.Pretty (encodePretty)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as LBS
-import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Functor
+import Data.Text hiding (init)
+import Development.Placeholders
+import Options.Applicative
+import Paths_scarf
+import System.Directory
+import System.Environment
+import System.Environment.XDG.BaseDir
+import System.Exit
+import System.FilePath
+import System.Process
 
 data EnvSpec = EnvSpec
-  {
-    envSpecPackages :: [Name]
+  { envSpecPackages :: [Name]
   }
 
 instance FromJSON EnvSpec where
@@ -31,16 +33,16 @@ instance FromJSON EnvSpec where
       <$> o .: "packages"
 
 instance ToJSON EnvSpec where
-  toJSON EnvSpec{..} = object
-    [
-      "packages" .= envSpecPackages
-    ]
+  toJSON EnvSpec {..} =
+    object
+      [ "packages" .= envSpecPackages
+      ]
 
 emptyEnvSpec :: EnvSpec
-emptyEnvSpec = EnvSpec
-  {
-    envSpecPackages = []
-  }
+emptyEnvSpec =
+  EnvSpec
+    { envSpecPackages = []
+    }
 
 prettyEnvSpec :: EnvSpec -> ByteString
 prettyEnvSpec = encodePretty
@@ -62,10 +64,11 @@ enterMyEnv enterCommand = do
   when hasConfigPath $ do
     outlink <- getUserCacheFile "scarf" "env/my-env-root"
     nixexpr <- getDataFileName "data/env.nix"
-    (ec, path_, _) <- readProcessWithExitCode
-      "nix-build"
-      [ nixexpr, "--arg", "config-file", "--output", outlink ]
-      ""
+    (ec, path_, _) <-
+      readProcessWithExitCode
+        "nix-build"
+        [nixexpr, "--arg", "config-file", "--output", outlink]
+        ""
     when (ec /= ExitSuccess) exitFailure -- TODO error message etc
     let path = init path_ </> "bin" -- Proper trimming etc.
     -- TODO what does "setting path" mean on Windows? Do we even have environments?
@@ -74,75 +77,90 @@ enterMyEnv enterCommand = do
         Nothing -> path
         Just p -> path ++ searchPathSeparator : p
     setEnv "PATH" newPATH
-  withCreateProcess enterCommand $ \ _ _ _ child ->
+  withCreateProcess enterCommand $ \_ _ _ child ->
     waitForProcess child >>= exitWith
 
 -- | This is implicitly pulled from the Scarf package set for now
-data Name = Name { name :: Text }
+data Name = Name {name :: Text}
 
 instance FromJSON Name where
   parseJSON = withText "Text" (pure . Name)
 
 instance ToJSON Name where
-  toJSON Name{name} = toJSON name
+  toJSON Name {name} = toJSON name
 
 data AddOpts = AddOpts
   { package :: Name
   }
 
 addOptions :: Parser AddOpts
-addOptions = AddOpts . Name <$>
-  strArgument
-    (  metavar "PKG"
-    <> help "the name of the package to add"
-    )
+addOptions =
+  AddOpts . Name
+    <$> strArgument
+      ( metavar "PKG"
+          <> help "the name of the package to add"
+      )
 
 data EnterOpts = EnterOpts
   { enterCommand :: CreateProcess
   }
 
 enterOptions :: Parser EnterOpts
-enterOptions = EnterOpts <$> (proc <$>
-  strArgument
-    (  metavar "PROG"
-    <> help "program to run in the environment"
-    )
-  <*>
-  many (strArgument
-         (  metavar "ARG"
-         <> help "arguments to the program to run"
-         )))
+enterOptions =
+  EnterOpts
+    <$> ( proc
+            <$> strArgument
+              ( metavar "PROG"
+                  <> help "program to run in the environment"
+              )
+            <*> many
+              ( strArgument
+                  ( metavar "ARG"
+                      <> help "arguments to the program to run"
+                  )
+              )
+        )
 
 data EnvCommand
   = Enter EnterOpts
   | Add AddOpts
 
 envOptions :: Parser EnvCommand
-envOptions = hsubparser
-  ((command "enter"
-    (info (Enter <$> enterOptions) mempty))
-  <>
-  (command "add"
-   (info (Add <$> addOptions) mempty)))-- progdesc?
+envOptions =
+  hsubparser
+    ( ( command
+          "enter"
+          (info (Enter <$> enterOptions) mempty)
+      )
+        <> ( command
+               "add"
+               (info (Add <$> addOptions) mempty) -- progdesc?
+           )
+    )
 
 data Command = Env EnvCommand
 
 options :: Parser Command
-options = hsubparser
-  (command "env"
-    (info (Env <$> envOptions) mempty)) -- progdesc?
+options =
+  hsubparser
+    ( command
+        "env"
+        (info (Env <$> envOptions) mempty) -- progdesc?
+    )
 
 optionsInfo :: ParserInfo Command
-optionsInfo = info (options <**> helper)
-  (  fullDesc
-  <> progDesc "The Scarf command-line tool"
-  )
+optionsInfo =
+  info
+    (options <**> helper)
+    ( fullDesc
+        <> progDesc "The Scarf command-line tool"
+    )
 
 main :: IO ()
 main = do
   Env ecmd <- execParser optionsInfo
   case ecmd of
-    Enter (EnterOpts { enterCommand }) ->
+    Enter (EnterOpts {enterCommand}) ->
       enterMyEnv enterCommand
-    Add (AddOpts { package }) ->
+    Add (AddOpts {package}) ->
       $notImplemented
