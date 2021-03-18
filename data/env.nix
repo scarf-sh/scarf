9481fab39e7ee6caf775b9be2ab5f39f8d0bdf93
Alex Biehl <alexbiehl@gmail.com>
2021-03-18 16:55:01 +0100

    Initial setup of environment

--- data/env.nix
+++ data/env.nix
@@ -1,1 +1,13 @@
-{ config-file }: throw "not implemented"
+{ config-file }:
+let
+  # TODO: Where should the Scarf package set come from?
+  pkgs = import <nixpkgs> {};
+
+  config = builtins.fromJSON (builtins.readFile config-file);
+
+  env = pkgs.buildEnv {
+    name = "my-env";
+    paths = map (package: pkgs.${package}) config.packages;
+  };
+in
+  env
